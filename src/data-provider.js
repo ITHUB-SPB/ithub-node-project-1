f5db72347befc02e2b7612924355d43941359de5
Трофимов Алексей <a.trofimov@domain.local>
2026-01-19 16:20:58 +0300

    start implementing connection via built-in sqlite module

--- src/data-provider.js
+++ src/data-provider.js
@@ -1,4 +1,28 @@
+import sqlite from 'node:sqlite'
 import bookings from './data.json' with { type: "json" }
+
+const connection = new sqlite.DatabaseSync('db.sqlite3')
+
+connection.exec(`create table if not exists bookings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    start INTEGER NOT NULL,
+    end INTEGER NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`)
+
+connection.exec(`insert into bookings (start, end) values (
+    1263168000,
+    1263172000
+)`)
+
+connection.exec(`insert into bookings (start, end) values (
+    1263180000,
+    1263192000
+)`)
+
+const selectAllBookings = connection.prepare('select * from bookings')
+console.log(selectAllBookings.all())
+
 
 export { bookings }
 
