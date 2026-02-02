fb7b1b31fe9d5056847e0ec9628c9a2d9e12b571
Lex Trofimov <daslef93@gmail.com>
2026-02-02 12:08:02 +0300

    контрольные точки 2,3

--- src/data-provider.js
+++ src/data-provider.js
@@ -25,8 +25,3 @@ console.log(selectAllBookings.all())
 
 
 export { bookings }
-
-// export const bookings = [
-//     { id: 1, start: 1263168000, end: 1263172000, createdAt: 1765450296 },
-//     { id: 2, start: 1263180000, end: 1263192000, createdAt: 1765450298 },
-// ]
