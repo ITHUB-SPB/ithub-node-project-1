af4c66e9d27a8c618a0aeea9ca2c8fb000ecacfc
1nterFekT <lavamishals@gmail.com>
2026-02-07 16:19:59 +0300

    DONE TASK 1

---
+++ src/database/cli/index.js
@@ -0,0 +1,33 @@
+import { createTables, resetTables } from './ddl.js'
+import { seedTables } from './seed.js';
+
+function cli() {
+    const command = process.argv[2];
+
+    if (command === 'create') {
+        try {
+            createTables();
+            console.log('Таблицы созданы (или уже присутствовали)');
+        } catch (error) {
+            console.error(`Ошибка при создании таблиц: ${error.message}`);
+        }
+    } else if (command === 'reset') {
+        try {
+            resetTables();
+            console.log('Данные из таблиц успешно удалены');
+        } catch (error) {
+            console.error(`Ошибка при сбросе таблиц: ${error.message}`);
+        }
+    } else if (command === 'seed') {
+        try {
+            seedTables();
+            console.log('Таблицы наполнены фейковыми данными');
+        } catch (error) {
+            console.error(`Ошибка при наполнении таблиц: ${error.message}`);
+        }
+    } else {
+        console.log('Команда не существует. Запросите help для информации');
+    }
+}
+
+cli();
